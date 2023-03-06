7634b8a3d706c63c8fc5e7b98f97054dfacafe2e
Joao <joaocarloslima@me.com>
2023-03-06 12:07:31 -0300

    🍃 spring framework

---
+++ src/main/java/br/com/fiap/gestanca/controllers/DespesaController.java
@@ -0,0 +1,29 @@
+package br.com.fiap.gestanca.controllers;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.fiap.gestanca.models.Despesa;
+
+@RestController
+public class DespesaController {
+
+    @GetMapping("/api/despesas")
+    public Despesa index(){
+       
+        Despesa despesa = new Despesa(
+                    new BigDecimal(100), 
+                    LocalDate.now(), 
+                    "Cinema"
+                );
+
+        return despesa;
+    }
+    
+}
